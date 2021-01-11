6e1a13aa25309220614b6e33836eb324bc213a19
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-01-11 12:38:41 -0300

    test: update tests after change .env.json file

--- index.spec.js
+++ index.spec.js
@@ -1,12 +1,12 @@
 const env = require('./index')
 
 describe('Environment', () => {
-  test('Should return undefined if env function is not called', () => {
-    expect(process.env.test).toBe(undefined)
-  })
+    test('Should return undefined if env function is not called', () => {
+        expect(process.env.test).toBe(undefined)
+    })
 
-  test('Should return env variables if env function is loaded', () => {
-    env()
-    expect(process.env.test).toBe('{"cukinha":"Cuka"}')
-  })
+    test('Should return env variables if env function is loaded', () => {
+        env()
+        expect(process.env.my_app_env).toBe('{"nodeEnv":"development","api":{"prefix":"/api"},"database":{"username":"foo","password":"bar","name":"awesome_db_name","host":"my_db_host"}}')
+    })
 })
